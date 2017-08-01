4e219674c583c03b260c6a98a8670320e86957a7
uniibu <unibtc@gmail.com>
2017-08-01 11:55:27 +0000

    refactor code for es6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,99 +1,111 @@
-var lb_usercode = $('#lb-usercode');
-var lb_code = document.getElementById('lb-code');
-var browser = false;
-var minified = false;
-var lb_usrtxt;
-var lb_txt;
-if (typeof (Worker) !== 'undefined') {
-  if (typeof (worker) === 'undefined') {
-    var worker = new Worker('assets/js/worker.js');
-  }
-  worker.addEventListener('message', e => {
-    var data = e.data;
-    switch (data.cmd) {
-      case 'lebab':
-        $('#lb-ver').prop('src', `https://img.shields.io/badge/lebab-${data.semver}-brightgreen.svg`);
-        start();
-        break;
-      case 'defaultCode':
-        initiate(data.code);
-        transform();
-        break;
-      case 'transformed':
-        lb_txt.setValue(data.code);
-        break;
+(($ => {
+  const o = {
+    lb_usercode: $('#lb-usercode'),
+    lb_code: $('#lb-code').get(0),
+    browser: false,
+    minified: false,
+    lb_usrtxt: null,
+    lb_txt: null,
+    worker: null
+  };
+  const f = {
+    fallback() {
+      const sc = document.createElement('script');
+      sc.src = 'assets/js/main-fallback.js';
+      document.body.appendChild(sc);
+    },
+    prepare() {
+      o.worker.postMessage({'cmd': 'prepare', 'msg': ''});
+    },
+    start() {
+      o.worker.postMessage({'cmd': 'start', 'msg': ''});
+    },
+    getOpts() {
+      const opts = [];
+      $('.lb-optionsbox input:checked').each((i, v) => {
+        opts.push($(v).siblings('.custom-control-description').text());
+      });
+      return opts;
+    },
+    transform(str) {
+      const lb_toTransform = str || o.lb_usrtxt.getValue();
+      const opts = this.getOpts();
+      o.worker.postMessage({'cmd': 'transform', 'msg': {'code': lb_toTransform, 'opts': opts, browser: o.browser, minified: o.minified}});
+    },
+    workerStart() {
+      o.worker.addEventListener('message', e => {
+        const data = e.data;
+        switch (data.cmd) {
+          case 'lebab':
+            $('#lb-ver').prop('src', `https://img.shields.io/badge/lebab-${data.semver}-brightgreen.svg`);
+            this.start();
+            break;
+          case 'defaultCode':
+            this.initiate(data.code);
+            this.transform();
+            break;
+          case 'transformed':
+            o.lb_txt.setValue(data.code);
+            break;
+        }
+      }, false);
+    },
+    initiate(c) {
+      const that = this;
+      o.lb_usercode.val(c.trim());
+      o.lb_usrtxt = CodeMirror.fromTextArea(o.lb_usercode.get(0), {
+        lineNumbers: true,
+        theme: 'panda-syntax',
+        lineWrapping: true,
+        mode: 'javascript',
+        showCursorWhenSelecting: true
+      });
+      o.lb_txt = CodeMirror.fromTextArea(o.lb_code, {
+        lineNumbers: true,
+        theme: 'panda-syntax',
+        lineWrapping: true,
+        mode: 'javascript',
+        readOnly: true
+      });
+      $('.lb-optionsbox').on('click', ({currentTarget, target}) => {
+        const $target = $(currentTarget);
+        $(target).blur();
+        const inp = $target.find('input');
+        inp.prop('checked', !inp.prop('checked'));
+        that.transform();
+        return false;
+      });
+      $('.cm-topminify').show();
+      $('.cm-topminify-es2015').show();
+      $('.lb-checkbox-es2015').on('click', function() {
+        $(this).toggleClass('checked');
+        if ($(this).hasClass('checked')) {
+          o.browser = true;
+        } else {
+          o.browser = false;
+        }
+        that.transform();
+      });
+      $('.lb-checkbox').on('click', function() {
+        $(this).toggleClass('checked');
+        if ($(this).hasClass('checked')) {
+          o.minified = true;
+        } else {
+          o.minified = false;
+        }
+        that.transform();
+      });
+      o.lb_usrtxt.on('change', ({doc}) => {
+        that.transform(doc.getValue());
+      });
     }
-  }, false);
-} else {
-  var sc = document.createElement('script');
-  sc.src = 'assets/js/main-fallback.js';
-  document.body.appendChild(sc);
-}
-function prepare() {
-  worker.postMessage({'cmd': 'prepare', 'msg': ''});
-}
-function start() {
-  worker.postMessage({'cmd': 'start', 'msg': ''});
-}
-function getOpts() {
-  var opts = [];
-  $('.lb-optionsbox input:checked').each((i, v) => {
-    opts.push($(v).siblings('.custom-control-description').text());
-  });
-  return opts;
-}
-function transform(str) {
-  var lb_toTransform = str || lb_usrtxt.getValue();
-  var opts = getOpts();
-  worker.postMessage({'cmd': 'transform', 'msg': {'code': lb_toTransform, 'opts': opts, browser, minified}});
-}
-function initiate(c) {
-  lb_usercode.val(c.trim());
-  lb_usrtxt = CodeMirror.fromTextArea(lb_usercode.get(0), {
-    lineNumbers: true,
-    theme: 'panda-syntax',
-    lineWrapping: true,
-    mode: 'javascript',
-    showCursorWhenSelecting: true
+  };
 
-  });
-  lb_txt = CodeMirror.fromTextArea(lb_code, {
-    lineNumbers: true,
-    theme: 'panda-syntax',
-    lineWrapping: true,
-    mode: 'javascript',
-    readOnly: true
-  });
-  $('.lb-optionsbox').on('click', e => {
-    var $target = $(e.currentTarget);
-    $(e.target).blur();
-    var inp = $target.find('input');
-    inp.prop('checked', !inp.prop('checked'));
-    transform();
-    return false;
-  });
-  $('.cm-topminify').show();
-  $('.cm-topminify-es2015').show();
-  $('.lb-checkbox-es2015').on('click', function() {
-    $(this).toggleClass('checked');
-    if ($(this).hasClass('checked')) {
-      browser = true;
-    } else {
-      browser = false;
-    }
-    transform();
-  });
-  $('.lb-checkbox').on('click', function() {
-    $(this).toggleClass('checked');
-    if ($(this).hasClass('checked')) {
-      minified = true;
-    } else {
-      minified = false;
-    }
-    transform();
-  });
-  lb_usrtxt.on('change', object => {
-    transform(object.doc.getValue());
-  });
-}
-prepare();
+  if (typeof (Worker) !== 'undefined') {
+    o.worker = new Worker('assets/js/worker.js');
+    f.workerStart();
+    f.prepare();
+  } else {
+    f.fallback();
+  }
+})(jQuery));
--- assets/js/worker.js
+++ assets/js/worker.js
@@ -1,6 +1,6 @@
 importScripts('https://unpkg.com/babel-standalone@6.25.0/babel.min.js', 'https://unpkg.com/babili-standalone@0.0.10/babili.min.js');
 self.addEventListener('message', e => {
-  var data = e.data;
+  const data = e.data;
   switch (data.cmd) {
     case 'prepare':
       loadLebab(ver => {
@@ -19,15 +19,19 @@ self.addEventListener('message', e => {
 }, false);
 
 function transform(code, opts, browser, min) {
-  var code = lebab.transform(code, opts);
-  code = lebab.transform(code.code, opts);
-  if (browser) {
-    code.code = transpile(code.code);
+  try {
+    var code = lebab.transform(code, opts);
+    code = lebab.transform(code.code, opts);
+    if (browser) {
+      code.code = transpile(code.code);
+    }
+    if (min) {
+      code.code = minify(code.code);
+    }
+    return code.code;
+  } catch (err) {
+    console.log('[Debug]', err);
   }
-  if (min) {
-    code.code = minify(code.code);
-  }
-  return code.code;
 }
 function transpile(inp) {
   return Babel.transform(inp, { presets: ['es2015'] }).code;
@@ -36,71 +40,43 @@ function minify(inp) {
   return Babili.transform(inp).code;
 }
 function loadLebab(cb) {
-  var myReq = new Request('https://umdfied.herokuapp.com/umdfied/lebab/latest');
+  const myReq = new Request('https://umdfied.herokuapp.com/umdfied/lebab/latest');
   fetch(myReq)
     .then(response => {
-      var rsp = response.json();
-      rsp.then(data => {
-        if (data.success === true) {
-          cb(data.message.semver);
-          importScripts(data.message.url);
+      const rsp = response.json();
+      rsp.then(({success, message}) => {
+        if (success === true) {
+          cb(message.semver);
+          importScripts(message.url);
         }
       });
     });
 }
 function defaultCode() {
-  var defaultCode = `
+  const defaultCode = `
 'use strict';
+var _ = require('lodash');
 
-// Let/const
-var name = 'Bob', time = 'yesterday';
-time = 'today';
+function Person(cfg) {
+  this.names = [cfg.fname, cfg.lname];
+}
+Person.prototype.greet = function(title) {
+  title = title || "Mr";
+  var fullName = this.names
+    .map(function(n) {
+      return _.upperFirst(n);
+    })
+    .join(" ");
 
-// Template string
-console.log('Hello ' + name + ', how are you ' + time + '?');
-
-var bob = {
-  // Object shorthand
-  name: name,
-  // Object method
-  sayMyName: function () {
-    console.log(this.name);
+  console.log("Hello " + title + " " + fullName + "!");
+};
+Person.prototype.greetWithAllTitles = function() {
+  for (var i = 0; i < arguments.length; i++) {
+    var title = arguments[i];
+    this.greet(title);
   }
 };
 
-// Classes
-var SkinnedMesh = function SkinnedMesh() {
-};
-
-SkinnedMesh.prototype.update = function (camera) {
-  camera = camera || createCamera();
-  this.camera = camera;
-};
-
-Object.defineProperty(SkinnedMesh.prototype, 'name', {
-  set: function (geometry) {
-    this.geometry = geometry;
-  },
-  get: function () {
-    return this.geometry;
-  }
-});
-
-// Commonjs
-var lebab = require('lebab');
-module.exports = SkinnedMesh;
-
-// Arrow functions
-var render = function () {
-  // ...
-  requestAnimationFrame(render);
-};
-
-// Arrow async functions
-var renderAsync = async function () {
-  // ...
-  await requestAnimationFrame(render);
-};
-    `;
+module.exports = Person;`;
   return defaultCode;
 }
