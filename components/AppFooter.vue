0e928e2be6b8bd881339557db0f0e3447c9fcedc
vhanni <bayanipsayoiii@gmail.com>
2018-06-11 21:04:34 +0800

    Add footer and Loading components

---
+++ components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+	<footer class="footer">Lebab - Distributed under MIT License</footer>
+</template>
+<style>
+	.footer {
+		border-top: 1px solid #e8e8e8;
+    	padding: 30px 0;
+    	text-align: center;
+	}
+</style>
---
+++ components/AppLoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="loading-page"></div>
+</template>
+
+<style scoped>
+  .loading-page {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url('/static/images/loader.gif') no-repeat center;
+    text-align: center;
+    padding-top: 200px;
+  }
+</style>
