2ff28257ffcee823123cea497a4c7efa3c1c62f9
vhanni <bayanipsayoiii@gmail.com>
2018-06-11 20:59:09 +0800

    Change href attribute

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,21 +1,22 @@
 <template>
-<b-navbar class="navbar-default" toggleable="md" type="light" fixed="top">
-  <b-container>
-  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+	<b-navbar class="navbar-default" toggleable="md" type="light" fixed="top">
+	  <b-container>
+	  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-  <b-navbar-brand class="logo" href="#"></b-navbar-brand>
+	  <b-navbar-brand class="logo" to="/"></b-navbar-brand>
 
-  <b-collapse is-nav id="nav_collapse">
-    <!-- Right aligned nav items -->
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item to="/dashboard">Try it live</b-nav-item>
-      <b-nav-item target="_blank" href="//github.com/uniibu/lebab-ce">Lebab</b-nav-item>
-      <b-nav-item target="_blank" href="//github.com/uniibu/lebab-ce/issues">Issues</b-nav-item>
-    </b-navbar-nav>
-  </b-collapse>
-</b-container>
-</b-navbar>
+	  <b-collapse is-nav id="nav_collapse">
+	    <!-- Right aligned nav items -->
+	    <b-navbar-nav class="ml-auto">
+	      <b-nav-item to="/dashboard">Try it live</b-nav-item>
+	      <b-nav-item target="_blank" href="//github.com/uniibu/lebab-ce">Lebab</b-nav-item>
+	      <b-nav-item target="_blank" href="//github.com/uniibu/lebab-ce/issues">Issues</b-nav-item>
+	    </b-navbar-nav>
+	  </b-collapse>
+	</b-container>
+	</b-navbar>
 </template>
+
 <style>
 .navbar {
 	border-bottom: 2px solid #323330;
@@ -30,7 +31,7 @@
 }
 
 .logo {
-	background: url('~/static/lebab-logo.png') no-repeat center;
+	background: url('/static/images/lebab-logo.png') no-repeat center;
 	background-size: auto 46px;
 	width: 60px;
 	height: 50px;
