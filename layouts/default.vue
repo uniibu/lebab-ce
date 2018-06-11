4fb213b4dd6d6fb8cdcb2b789a1de543e7f4712d
vhanni <bayanipsayoiii@gmail.com>
2018-06-11 20:56:52 +0800

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-     <app-navbar/>
+    <app-navbar/>
     <nuxt/>
+    <app-footer/>
   </div>
 </template>
 <script>
 import AppNavbar from '~/components/AppNavbar.vue';
+import AppFooter from '~/components/AppFooter.vue';
 export default {
   components: {
-    AppNavbar
+    AppNavbar,
+    AppFooter
   }
 };
 </script>
