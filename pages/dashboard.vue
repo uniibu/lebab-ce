15b56d661575b7f0e5fa1e21795baaf16f141f86
vhanni <bayanipsayoiii@gmail.com>
2018-06-11 20:55:06 +0800

    Edited remove exported head in components

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -5,18 +5,11 @@
     </b-container>
   </b-jumbotron>
 </template>
-
 <script>
-import LebabEditor from '~/components/LebabEditor.vue';
-export default {
-  components: {
-    LebabEditor
-  },
-  head: {
-    script: [
-      { src: 'https://unpkg.com/babel-standalone@6.25.0/babel.min.js' },
-      { src: 'https://unpkg.com/babili-standalone@0.0.10/babili.min.js' }
-    ]
-  }
-};
+	import LebabEditor from '~/components/LebabEditor.vue'
+	export default {
+		components: {
+			LebabEditor
+		}
+	}
 </script>
