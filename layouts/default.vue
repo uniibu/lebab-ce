a8cc23be04a21050dd9b4c6394e524bb72aaff65
vhanni <bayanipsayoiii@gmail.com>
2018-06-12 22:29:57 +0800

    Remove loading

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <app-navbar/>
-    <no-ssr>
-      <app-loading slot="placeholder" />
-    </no-ssr>
     <nuxt/>
     <app-footer/>
   </div>
@@ -11,32 +8,10 @@
 <script>
 import AppNavbar from '~/components/AppNavbar.vue';
 import AppFooter from '~/components/AppFooter.vue';
-import AppLoading from '~/components/AppLoading.vue'
 export default {
   components: {
     AppNavbar,
-    AppFooter,
-    AppLoading
+    AppFooter
   }
 };
 </script>
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-</style>
