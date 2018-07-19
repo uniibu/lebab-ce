dac4245a8a39c6842c91650689e9c405a8ae02c4
uni <unibtc@gmail.com>
2018-07-19 11:50:11 +0800

    remove un-needed lebab module and plugin

--- modules/lebabModule.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = function nuxtLebab (moduleOptions) {
-  this.addPlugin({
-    src: path.resolve(__dirname, 'lebabPlugin.template.js'),
-    fileName: 'lebab.js',
-    options: moduleOptions
-  });
-};
