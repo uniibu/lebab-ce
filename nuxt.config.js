5ba63c27f8bfa614dfe1db8b1cc35a39088083bb
unibtc <unibtc@gmail.com>
2019-03-05 11:53:00 +0000

    integrate build.js to nuxt.config
    Signed-off-by: unibtc <unibtc@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,19 @@
 import conf from './config';
-export default {
+import axios from 'axios';
+import consola from 'consola';
+import fs from 'fs';
+
+const getLebab = async () => {
+  consola.info('Fetching latest Lebab version');
+  const { data: { message: msg } } = await axios.get('https://umdfied.herokuapp.com/umdfied/lebab/latest');
+  if (!msg || !msg.url || !msg.semver) {
+    throw new Error('Could not fetch lebab');
+  }
+  consola.info(`Using lebab@${msg.semver}`);
+  return msg;
+};
+
+const nuxtConfig = {
   mode: conf.mode,
   head: conf.head,
   css: conf.css,
@@ -29,5 +43,26 @@ export default {
         });
       }
     }
+  },
+  hooks: {
+    generate: {
+      page(ctx) {
+        const r = /(\.css" rel="preload" as=")(script)/gm;
+        ctx.html = ctx.html.replace(r, '$1style');
+        return ctx;
+      },
+      done() {
+        fs.writeFileSync('docs/CNAME', 'lebab.unibtc.me');
+      }
+    }
   }
 };
+
+export default async function nuxt() {
+  const { url, semver } = await getLebab();
+  nuxtConfig.env = Object.assign(nuxtConfig.env || {}, { lebab: semver });
+  nuxtConfig.head.link.push({ href: url, rel: 'preload', as: 'script' });
+  nuxtConfig.head.script.push({ src: url, type: 'text/javascript', body: true });
+  return nuxtConfig;
+}
+
